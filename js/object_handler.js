3d8a162912cc704676d83b7b5f1faa6556faa394
Zack <Zack Hardy>
2016-10-23 14:03:55 -0500

    adding knockout, creating knockout view model, using knockout for options within chosen

--- js/object_handler.js
+++ js/object_handler.js
@@ -1,5 +1,83 @@
+function SqlBuildModel(){
+	var self = this;
+	self.dataTypes = [
+		{
+			name: "smallint"
+		},
+		{
+			name: "integer"
+		},
+		{
+			name: "bigint"
+		},
+		{
+			name:"decimal"
+		},
+		{
+			name:"numeric"
+		},
+		{
+			name:"real"
+		},
+		{
+			name:"double"
+		},
+		{
+			name:"smallserial"
+		},
+		{
+			name:"serial"
+		},
+		{
+			name:"bigserial"
+		},
+		{
+			name:"money"
+		},
+		{
+			name:"varchar",
+			length:80
+		},
+		{
+			name:"char",
+		},
+		{
+			name:"text"
+		},
+		{
+			name:"timestamp without time zone"
+		},
+		{
+			name:"timestramp with time zone"
+		},
+
+		{
+			name:"date"
+		},
+		{
+			name:"time without time zone",
+		},
+		{
+			name:"time with time zone"
+		},
+		{
+			name:"interval"
+		},
+		{
+			name:"boolean",
+			values: [ 
+			true,
+			false
+			]
+		},
+		{
+			name:"Custom (enum)"
+		}
+	]
+}
+
+
 $(document).ready(function(){
-	console.log('test');
-	$("#chosen-select").chosen();
+	ko.applyBindings(new SqlBuildModel());
 	$('.chosen-select').chosen();
 });
