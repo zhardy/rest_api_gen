d51a678a8dfac24cf50ebf99b0f8d5c89e1e0fbf
Zack <Zack Hardy>
2016-11-20 10:29:28 -0600

    some python script changes that need to be on the python_script branch. oops

--- python/test.py
+++ python/test.py
@@ -1,2 +1,17 @@
+import os, json
 from lxml import etree
-print("running")
+from pprint import pprint
+
+
+with open('/home/zack/Downloads/data.json') as data_file:
+	data = json.load(data_file);
+for table in data:
+	print table["name"]
+#print data[0]["values"][0]["isReference"]
+
+# f = open("/home/zack/Downloads/data.json", "r")
+
+# for file in os.listdir("/home/zack/Downloads/"):
+# 	print file
+
+# print f.read()
