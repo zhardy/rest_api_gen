39429f354a19a029d2de993621c8d0424d13358a
Zack Hardy <zhardy177@gmail.com>
2016-12-18 21:29:55 -0600

    custom values now work with python script to generate sql

--- python/db_architecture.sql
+++ python/db_architecture.sql
@@ -1,29 +1,40 @@
-select 'drop table if exists "' || tablename || '" cascade;' from pg_tables;
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 
-create type writer_type as enum ('Fiction', 'Non-Fiction', 'Poetry');
-create type staffing_type as enum ('Writer-in-Residence', 'Faculty');
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+
+
+create type writer_type as enum ('Fiction','Non-Fiction','Poetry');
+create type staffing_type as enum ('Writer-in-Residence','Faculty');
 
 create table Users(
 UserID serial,
-User varchar,
+Username varchar(80),
 primary key (UserID)
 );
 
 create table Passwords(
 PasswordID serial,
-Password varchar,
+Password varchar(80),
 primary key (PasswordID)
 );
 
 create table Content(
 ContentID serial,
-Description varchar,
+Description varchar(80),
 primary key (ContentID)
+);
+
+create table Images(
+ImageID serial,
+ImagePath varchar(80),
+primary key (ImageID)
 );
 
 create table Writers(
 WriterID serial,
-WriterName varchar,
+WriterName varchar(80),
 WriterType writer_type,
 StaffingType staffing_type,
 ImageID serial,
@@ -35,7 +46,7 @@ primary key (WriterID)
 
 create table CraftSessions(
 CraftSessionID serial,
-CraftSessionName varchar,
+CraftSessionName varchar(80),
 ContentID serial,
 WriterID serial,
 foreign key(ContentID) references Content,
@@ -53,18 +64,12 @@ primary key (ReadingID)
 
 create table Workshops(
 WorkshopID serial,
-WorkshopName varchar,
+WorkshopName varchar(80),
 ContentID serial,
 WriterID serial,
 foreign key(ContentID) references Content,
 foreign key(WriterID) references Writers,
 primary key (WorkshopID)
-);
-
-create table Images(
-ImageID serial,
-ImagePath varchar,
-primary key (ImageID)
 );
 
 create table UserToPassword(
@@ -78,8 +83,8 @@ primary key (UserToPasswordID)
 
 create table Sponsors(
 SponsorID serial,
-SponsorName varchar,
-Link varchar,
+SponsorName varchar(80),
+Link varchar(80),
 ContentID serial,
 foreign key(ContentID) references Content,
 primary key (SponsorID)
--- python/test.py
+++ python/test.py
@@ -6,7 +6,11 @@ FIRST_LINE = """DROP SCHEMA public CASCADE;\n
 CREATE SCHEMA public;\n
 GRANT ALL ON SCHEMA public TO postgres;\n
 GRANT ALL ON SCHEMA public TO public;\n"""
-CREATE_TABLE_BEGIN = "create table ";
+CREATE_TABLE_BEGIN = "create table "
+CREATE_CUSTOM_BEGIN = "create type "
+AS_ENUM = " as enum "
+OPEN_QUOTE = "'"
+CLOSED_QUOTE = "'"
 OPEN_PARAN = "("
 CLOSED_PARAN = ")"
 SEMI = ";"
@@ -17,7 +21,7 @@ FOREIGN_KEY = "foreign key"
 REF = " references "
 LINEBR = "\n";
 
-filepath = '/home/zack/Downloads/data (1).json'
+filepath = '/home/zack/Downloads/data (6).json'
 
 if len(sys.argv) > 1:
 	filepath = sys.argv[1]
@@ -29,26 +33,35 @@ db_arch = open('db_architecture.sql', 'w+')
 db_arch.write(FIRST_LINE + LINEBR + LINEBR)
 
 for table in data:
-	db_arch.write(CREATE_TABLE_BEGIN + table["name"] + OPEN_PARAN + LINEBR)
-	primary = None
-	foreign = []
+	if "type" in table and table["type"] == "Custom":
+		db_arch.write(CREATE_CUSTOM_BEGIN + table["name"] + AS_ENUM + OPEN_PARAN)
+		for value in table["values"]:
+			if value != table["values"][len(table["values"])-1]:
+				db_arch.write(OPEN_QUOTE + value + CLOSED_QUOTE + COMMA)
+			else:
+				db_arch.write(OPEN_QUOTE + value + CLOSED_QUOTE + CLOSED_PARAN + SEMI + LINEBR + LINEBR)
 
-	for value in table["values"]:
-		if "length" in value["type"] and len(value["type"]["length"]) != 0:
-			db_arch.write(value["name"] + SPACE + value["type"]["name"] + OPEN_PARAN + value["type"]["length"] + CLOSED_PARAN + COMMA + LINEBR)
-		else:
-			db_arch.write(value["name"] + SPACE + value["type"]["name"] + COMMA + LINEBR)
+	else:
+		db_arch.write(CREATE_TABLE_BEGIN + table["name"] + OPEN_PARAN + LINEBR)
+		primary = None
+		foreign = []
 
-		if value["isPrimary"]:
-			primary = value
+		for value in table["values"]:
+			if "length" in value["type"] and value["type"]["length"] != 0:
+				db_arch.write(value["name"] + SPACE + value["type"]["name"] + OPEN_PARAN + str(value["type"]["length"]) + CLOSED_PARAN + COMMA + LINEBR)
+			else:
+				db_arch.write(value["name"] + SPACE + value["type"]["name"] + COMMA + LINEBR)
 
-		if value["isReference"]:
-			foreign.append(value)
+			if value["isPrimary"]:
+				primary = value
 
-	for key in foreign:
-		db_arch.write(FOREIGN_KEY + OPEN_PARAN + key["name"] + CLOSED_PARAN + REF + key["foreignTable"] + COMMA + LINEBR)
+			if value["isReference"]:
+				foreign.append(value)
 
-	if primary != None:
-		db_arch.write(PRIMARY_KEY + OPEN_PARAN + primary["name"] + CLOSED_PARAN + LINEBR)
+		for key in foreign:
+			db_arch.write(FOREIGN_KEY + OPEN_PARAN + key["name"] + CLOSED_PARAN + REF + key["foreignTable"] + COMMA + LINEBR)
 
-	db_arch.write(CLOSED_PARAN + SEMI + LINEBR + LINEBR)
+		if primary != None:
+			db_arch.write(PRIMARY_KEY + OPEN_PARAN + primary["name"] + CLOSED_PARAN + LINEBR)
+
+		db_arch.write(CLOSED_PARAN + SEMI + LINEBR + LINEBR)
