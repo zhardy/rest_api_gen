53955a2fa5facd2262f04ca4f169cc78b79fbfd5
Zack Hardy <zhardy177@gmail.com>
2016-12-21 22:04:20 -0600

    preliminary begining of JavaScript routes, and first constants

--- python/script.py
+++ python/script.py
@@ -1,28 +1,47 @@
 import sys, os, json, subprocess
 
 
-def sql_schema(filepath):
+	OPEN_QUOTE = "'"
+	CLOSED_QUOTE = "'"
+	OPEN_PARAN = "("
+	CLOSED_PARAN = ")"
+	OPEN_BRACKET = "{"
+	CLOSED_BRACKET = "}"
+	SEMI = ";"
+	SPACE = " "
+	COMMA = ","
+	LINEBR = "\n";
+	FW_SLASH = "/"
+	BW_SLASH = "\\"
 
-
-	#Constants
-	FIRST_LINE = """DROP SCHEMA public CASCADE;\n
+	BEGIN_SCHEMA = """DROP SCHEMA public CASCADE;\n
 CREATE SCHEMA public;\n
 GRANT ALL ON SCHEMA public TO postgres;\n
 GRANT ALL ON SCHEMA public TO public;\n"""
 	CREATE_TABLE_BEGIN = "create table "
 	CREATE_CUSTOM_BEGIN = "create type "
 	AS_ENUM = " as enum "
-	OPEN_QUOTE = "'"
-	CLOSED_QUOTE = "'"
-	OPEN_PARAN = "("
-	CLOSED_PARAN = ")"
-	SEMI = ";"
-	SPACE = " "
-	COMMA = ","
 	PRIMARY_KEY = "primary key "
 	FOREIGN_KEY = "foreign key"
 	REF = " references "
-	LINEBR = "\n";
+
+	BEGIN_ROUTES = """var express = require('express');\n
+var router = express.Router();"""
+	ROUTER_GET = "router.get('/"
+	ROUTER_POST = "router.post('/"
+	ROUTER_PUT = "router.put('/"
+	ROUTER_PATCH = "router.patch('/"
+
+	ROUTER_FUNCTION_BEGIN = "function(req, res) { "
+	RES_JSON = "res.json"
+	MODULE_EXPORTS = "module.exports = router;"
+
+
+def sql_schema(filepath):
+
+
+	#Constants
+
 
 	#Get JSON
 	with open(filepath) as data_file:
@@ -30,12 +49,12 @@ GRANT ALL ON SCHEMA public TO public;\n"""
 
 	#Create SQL schema document
 	db_arch = open('db_architecture.sql', 'w+')
-	db_arch.write(FIRST_LINE + LINEBR + LINEBR)
+	db_arch.write(BEGIN_SCHEMA + LINEBR + LINEBR)
 
 	#For each table in the data dictionary
 	for table in data:
 		#If type exists as a property of this table and the type property equals custom
-		if "type" in table and table["type"] == "Custom":
+		if "type" not in table and table["type"] == "Custom":
 			#Write out a custom enumerator with each value belonging in this table
 			db_arch.write(CREATE_CUSTOM_BEGIN + table["name"] + AS_ENUM + OPEN_PARAN)
 			for value in table["values"]:
@@ -72,9 +91,18 @@ GRANT ALL ON SCHEMA public TO public;\n"""
 
 
 def rest_api_gen(filepath, shell_path, location_for_api):
+
+
 	subprocess.call([shell_path, location_for_api])
-	# with open(filepath) as data_file:
-	# 	data = json.load(data_file)
+	with open(filepath) as data_file:
+		data = json.load(data_file)
+	
+	for table in data:
+		if "type" not in table:
+			js_route = open(location_for_api + "/" + table["name"], 'w')
+			js_route.write(BEGIN_ROUTES +LINEBR + LINEBR)
+			js_route.write(ROUTER_GET + table["name"] + COMMA + ROUTER_FUNCTION_BEGIN + LINEBR)
+			js_route.write()
 
 
 
@@ -84,7 +112,6 @@ def main():
 		filepath = raw_input("Please provide a filepath for the JSON:\n")
 		shell_path = raw_input("Please provide a filepath for the shell script:\n")
 		directory = raw_input("Please provide a filepath to create your new REST API:\n")
-
 	else:
 		filepath = sys.argv[1]
 		shell_path = sys.argv[2]
