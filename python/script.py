f1fcba8d0df53f7727238d578dc17930ab09cf56
zhardy <zhardy177@gmail.com>
2016-12-30 16:34:10 -0500

    updating script to use ~ in filepaths (when running in Linux); updating script to include JS at end of route filepath; updating script to close string/name of route; updating script to place all routes in routes folder

--- python/script.py
+++ python/script.py
@@ -1,40 +1,40 @@
 import sys, os, json, subprocess
 
 
-	OPEN_QUOTE = "'"
-	CLOSED_QUOTE = "'"
-	OPEN_PARAN = "("
-	CLOSED_PARAN = ")"
-	OPEN_BRACKET = "{"
-	CLOSED_BRACKET = "}"
-	SEMI = ";"
-	SPACE = " "
-	COMMA = ","
-	LINEBR = "\n";
-	FW_SLASH = "/"
-	BW_SLASH = "\\"
+OPEN_QUOTE = "'"
+CLOSED_QUOTE = "'"
+OPEN_PARAN = "("
+CLOSED_PARAN = ")"
+OPEN_BRACKET = "{"
+CLOSED_BRACKET = "}"
+SEMI = ";"
+SPACE = " "
+COMMA = ","
+LINEBR = "\n";
+FW_SLASH = "/"
+BW_SLASH = "\\"
 
-	BEGIN_SCHEMA = """DROP SCHEMA public CASCADE;\n
+BEGIN_SCHEMA = """DROP SCHEMA public CASCADE;\n
 CREATE SCHEMA public;\n
 GRANT ALL ON SCHEMA public TO postgres;\n
 GRANT ALL ON SCHEMA public TO public;\n"""
-	CREATE_TABLE_BEGIN = "create table "
-	CREATE_CUSTOM_BEGIN = "create type "
-	AS_ENUM = " as enum "
-	PRIMARY_KEY = "primary key "
-	FOREIGN_KEY = "foreign key"
-	REF = " references "
+CREATE_TABLE_BEGIN = "create table "
+CREATE_CUSTOM_BEGIN = "create type "
+AS_ENUM = " as enum "
+PRIMARY_KEY = "primary key "
+FOREIGN_KEY = "foreign key"
+REF = " references "
 
-	BEGIN_ROUTES = """var express = require('express');\n
+BEGIN_ROUTES = """var express = require('express');\n
 var router = express.Router();"""
-	ROUTER_GET = "router.get('/"
-	ROUTER_POST = "router.post('/"
-	ROUTER_PUT = "router.put('/"
-	ROUTER_PATCH = "router.patch('/"
+ROUTER_GET = "router.get('/"
+ROUTER_POST = "router.post('/"
+ROUTER_PUT = "router.put('/"
+ROUTER_PATCH = "router.patch('/"
 
-	ROUTER_FUNCTION_BEGIN = "function(req, res) { "
-	RES_JSON = "res.json"
-	MODULE_EXPORTS = "module.exports = router;"
+ROUTER_FUNCTION_BEGIN = "function(req, res) { "
+RES_JSON = "res.json"
+MODULE_EXPORTS = "module.exports = router;"
 
 
 def sql_schema(filepath):
@@ -90,29 +90,41 @@ def rest_api_gen(filepath, shell_path, location_for_api):
 
 
 	subprocess.call([shell_path, location_for_api])
+	print filepath
 	with open(filepath) as data_file:
 		data = json.load(data_file)
 	
 	for table in data:
 		if "type" not in table:
-			js_route = open(location_for_api + "/" + table["name"], 'w')
+			js_route = open(location_for_api + "/routes/" + table["name"] + ".js", 'w')
 			js_route.write(BEGIN_ROUTES + LINEBR + LINEBR)
-			js_route.write(ROUTER_GET + table["name"] + COMMA + ROUTER_FUNCTION_BEGIN + LINEBR)
-			js_route.write()
+			js_route.write(ROUTER_GET + table["name"] + CLOSED_QUOTE + COMMA + ROUTER_FUNCTION_BEGIN + LINEBR)
+			#js_route.write()
 
 
 
 
 def main():
-	if len(sys.argv) < 4:
-		filepath = raw_input("Please provide a filepath for the JSON:\n")
-		shell_path = raw_input("Please provide a filepath for the shell script:\n")
-		directory = raw_input("Please provide a filepath to create your new REST API:\n")
-	else:
-		filepath = sys.argv[1]
-		shell_path = sys.argv[2]
-		directory = sys.argv[3]
-	#sql_schema(filepath)
+	# if len(sys.argv) < 4:
+	# 	filepath = raw_input("Please provide a filepath for the JSON:\n")
+	# 	shell_path = raw_input("Please provide a filepath for the shell script:\n")
+	# 	directory = raw_input("Please provide a filepath to create your new REST API:\n")
+	# else:
+	# 	filepath = sys.argv[1]
+	# 	shell_path = sys.argv[2]
+	# 	directory = sys.argv[3]
+	# sql_schema(filepath)
+
+	filepath = "~/Downloads/data.json"
+	shell_path = "~/programming/rest_api_gen/node/node.sh"
+	directory = "~/programming/rest_api_gen/test"
+
+	if "~" in filepath:
+		filepath = os.path.expanduser(filepath)
+		shell_path = os.path.expanduser(shell_path)
+		directory = os.path.expanduser(directory)
+
+
 	rest_api_gen(filepath, shell_path, directory)
 
 main()
